fa1afa36a12196c84fbe271f66ffb4b5e8930441
Diogo Meira <diogomeira81@gmail.com>
2024-02-04 18:37:50 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ function App() {
 
   const [value,setValue] = useState('123')
 
+  const buttons = [
+    [7,8,9,"/"],
+    [4,5,6,"*"],
+    [1,2,3,"+"],
+    ['.',0,'=','-']
+  ]
+
+  const renderedButtons = buttons.map((row) => {
+     return <div className='flex justify-between mt-2'>{row.map((btn,i) => <Button>{btn}</Button>)}</div>
+  })
+
+  const handleClear = () => {
+    setValue('')
+  }
 
 
 
   return (
+
+    
     <div className="App">
       <div className=' flex justify-center mt-20 text-4xl'>
         <h2 className='text-blue-600'>React Calculator</h2>
@@ -20,39 +36,14 @@ function App() {
 
 
       <section id="Calculator" className='flex justify-center '>
-        <div className='mt-48 border border-black w-96 h-140 p-4 rounded bg-gray-500'>
+        <div className='mt-48 border border-black w-96 h-140 p-4 rounded-lg bg-gray-500'>
 
           <div id='Display' className='border bg-gray-800 text-white rounded border-black h-20'>{value}</div>
 
-          <div className='flex justify-between mt-2'>
-            <Button>7</Button >
-            <Button>8</Button >
-            <Button>9</Button >
-            <Button>/</Button >
-          </div>
-
-          <div className='flex justify-between'>
-            <Button>4</Button >
-            <Button>5</Button >
-            <Button>6</Button >
-            <Button className=''>*</Button >
-          </div>
-
-          <div className='flex justify-between'>
-            <Button>1</Button >
-            <Button>2</Button >
-            <Button>3</Button >
-            <Button>+</Button >
-          </div>
-          <div className='flex justify-between'>
-            <Button>.</Button >
-            <Button>0</Button >
-            <Button>=</Button >
-            <Button>-</Button >
-          </div>
+          {renderedButtons}
 
           <div>
-            <button className='border border-black rounded w-full bg-red-600 text-white h-14 mt-2'>Clear</button>
+            <button onClick={handleClear} className='border border-black rounded-lg w-full bg-red-600 text-white h-14 mt-2'>Clear</button>
           </div>
         </div>
       </section>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-export function Button ({children}) {
-    return <button className='border ml-0 mr-0 border-black rounded w-1/5 h-14 m-1 bg-blue-400 text-white' >{children}</button>
+export function Button ({children, handleClick}) {
+    return <button className='border ml-0 mr-0 border-black rounded-lg w-1/5 h-14 m-1 bg-blue-400 text-white' >{children}</button>
 }
 
